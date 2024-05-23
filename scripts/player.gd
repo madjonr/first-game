b4667982d796e4077493da86a96e37b773a031e9
madjonr <madjonr@gmail.com>
2024-05-23 18:01:03 +0800

    V0.1 增加移动同步

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,21 +8,17 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var player_id
+var direction = 1
+var do_jump = false
+var _is_on_floor = false
+
+@export var player_id:int = 1:
+	set(id):
+		player_id = id
+		%InputSynchronizer.set_multiplayer_authority(id)
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
-	
+func _apply_animations(delta):
 	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -37,7 +33,19 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	
+
+
+func _apply_movement_from_input(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if do_jump and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction = %InputSynchronizer.input_direction
 	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
@@ -45,3 +53,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _physics_process(delta):
+	# 这里其实是不信任任何客户端的操作，客户端只是将鼠标键盘的动作同步到服务端，
+	# 服务端（验证输入的有效性后）将动作同步回客户端，客户端再来显示所有移动效果。
+	# 所以这里就是要判断is_server的原因
+	if multiplayer.is_server():
+		_apply_movement_from_input(delta)
+	_apply_animations(delta)
